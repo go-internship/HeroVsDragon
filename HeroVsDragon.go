c4239077006ea89896d9fcb7fa437c58dd8b3c70
Sultan Moldobaev <gc738@mail.ru>
2019-06-20 14:27:12 +0600

    Removed EN lang
    Fixed #21 #19 #16
    1. Removed EN lang from code
    2. Removed their structs
    3. Some func renamed, code refactored

--- HeroVsDragon.go
+++ HeroVsDragon.go
@@ -41,17 +41,10 @@ type gameDataText struct { //Игровые тексты
 	gameOver, selWeapon, weapon1, weapon2, weapon3, winner, step, standoff string
 }
 
-type gameDataLogic struct { //Игровая логика
-
-}
-
-func setGetMainMenuTextRU() menuData { //Тексты главного меню
+func setGetMainMenuText() menuData { //Тексты главного меню
 	mainMenu := menuData{
 		point1:       "1. Начать новую игру",
-		point2:       "2. Выбрать язык",
-		point3:       "3. Выход",
-		langRU:       "1. Русский",
-		langEN:       "2. English",
+		point2:       "2. Выход",
 		loading:      "Загружается...",
 		bye:          "До скорой встречи!",
 		incorrectInp: "Неверный выбор, повторите снова",
@@ -59,19 +52,7 @@ func setGetMainMenuTextRU() menuData { //Тексты главного меню
 	return mainMenu
 }
 
-func setGetMainMenuTextEN() menuData { //Тексты главного меню
-	mainMenu := menuData{
-		point1:       "1. Start a new game",
-		point2:       "2. Choose lang",
-		point3:       "3. Exit",
-		loading:      "Loading...",
-		bye:          "Good bye :)",
-		incorrectInp: "Incorrect selection, try again",
-	}
-	return mainMenu
-}
-
-func setGetGameDataTextRU() gameDataText { //Игровые тексты
+func setGetGameDataText() gameDataText { //Игровые тексты
 	data := gameDataText{
 		hp:               "hp",
 		dragonMiss:       "Дракон промахнулся и не нанёс вам урона :)",
@@ -92,64 +73,9 @@ func setGetGameDataTextRU() gameDataText { //Игровые тексты
 	return data
 }
 
-func setGetGameDataTextEN() gameDataText { //Игровые тексты
-	data := gameDataText{
-		hp:               "hp",
-		dragonMiss:       "Dragon missed, no harm to Hero :)",
-		harmHeroToDragon: "You damaged Dragon to",
-		harmDragonToHero: "Dragon damaged You to",
-		hero:             "Hero",
-		dragon:           "Dragon Dragoner",
-		entHeroName:      "Enter Hero name: (press Enter to generate random)",
-		gameOver:         "Game over",
-		selWeapon:        "Select weapon:",
-		weapon1:          "1. Sword",
-		weapon2:          "2. Arrow",
-		weapon3:          "3. Firestone",
-		winner:           "Winner is",
-		step:             "Step #",
-		standoff:         "Friendship won :)",
-	}
-	return data
-}
-
-func setGetGameDataLogic() gameDataLogic { //Игровая логика
-	data := gameDataLogic{}
-	return data
-}
-
-func showLangMenu() {
-	fmt.Println(setGetMainMenuTextRU().langRU)
-	fmt.Println(setGetMainMenuTextRU().langEN)
-}
-
 func showMainMenu() {
-	fmt.Println(checkLangShowText(setGetMainMenuTextRU().point1, setGetMainMenuTextEN().point1))
-	fmt.Println(checkLangShowText(setGetMainMenuTextRU().point2, setGetMainMenuTextEN().point2))
-	fmt.Println(checkLangShowText(setGetMainMenuTextRU().point3, setGetMainMenuTextEN().point3))
-}
-
-func selectLangMenuItem() {
-	someThing := bufio.NewScanner(os.Stdin)
-	someThing.Scan()
-	inputLangMenuItem = strings.TrimSpace(someThing.Text()) //Убирает пробелы в начале и в конце
-	switch inputLangMenuItem {
-	case "1":
-		selectedMenuLang = true
-	case "2":
-		selectedMenuLang = false
-	default:
-		fmt.Println(checkLangShowText(setGetMainMenuTextRU().incorrectInp, setGetMainMenuTextEN().incorrectInp))
-		showLangMenu()
-		selectLangMenuItem()
-	}
-}
-
-func checkLangShowText(textRU, textEN string) string {
-	if selectedMenuLang == true {
-		return textRU
-	}
-	return textEN
+	fmt.Println(setGetMainMenuText().point1)
+	fmt.Println(setGetMainMenuText().point2)
 }
 
 func selectMainMenuItem() {
@@ -158,22 +84,19 @@ func selectMainMenuItem() {
 	inputMainMenuItem = strings.TrimSpace(someThing.Text()) //Убирает пробелы в начале и в конце
 	switch inputMainMenuItem {
 	case "1": //Начать новую игру
-		fmt.Println(checkLangShowText(setGetMainMenuTextRU().loading, setGetMainMenuTextEN().loading))
+		fmt.Println(setGetMainMenuText().loading)
 		isGameStart = true
-	case "2": //Выбрать язык
-		showLangMenu()
-		selectLangMenuItem()
-	case "3": //Выход
-		fmt.Println(checkLangShowText(setGetMainMenuTextRU().bye, setGetMainMenuTextEN().bye))
+	case "2": //Выход
+		fmt.Println(setGetMainMenuText().bye)
 		os.Exit(0)
 	default:
-		fmt.Println(checkLangShowText(setGetMainMenuTextRU().incorrectInp, setGetMainMenuTextEN().incorrectInp))
+		fmt.Println(setGetMainMenuText().incorrectInp)
 		selectMainMenuItem()
 	}
 }
 
 func gameStart() {
-	fmt.Println(checkLangShowText(setGetGameDataTextRU().entHeroName, setGetGameDataTextEN().entHeroName))
+	fmt.Println(setGetGameDataText().entHeroName)
 	inputHeroName()
 	for i := 0; ; i++ {
 		if !isGameEnd {
@@ -212,13 +135,13 @@ func inputHeroName() {
 }
 
 func showGameResult() {
-	fmt.Println(checkLangShowText(setGetGameDataTextRU().hero, setGetGameDataTextEN().hero), heroName,
-		"\t\t\t", checkLangShowText(setGetGameDataTextRU().dragon, setGetGameDataTextEN().dragon))
+	fmt.Println(setGetGameDataText().hero, heroName,
+		"\t\t\t", setGetGameDataText().dragon)
 	showCurrentHP()
 }
 
 func showCurrentHP() {
-	fmt.Println(hpHero, setGetGameDataTextRU().hp, "\t\t\t\t", hpDragon, setGetGameDataTextEN().hp)
+	fmt.Println(hpHero, setGetGameDataText().hp, "\t\t\t\t", hpDragon)
 }
 
 func checkCurrentHp() {
@@ -228,35 +151,33 @@ func checkCurrentHp() {
 }
 
 func showStep(step int) {
-	fmt.Println(checkLangShowText(setGetGameDataTextRU().step, setGetGameDataTextEN().step), step+1)
+	fmt.Println(setGetGameDataText().step, step+1)
 }
 
 func gameEnd() {
 	fmt.Println("\n\n")
-	fmt.Println(checkLangShowText(setGetGameDataTextRU().gameOver, setGetGameDataTextEN().gameOver))
+	fmt.Println(setGetGameDataText().gameOver)
 	showGameResult()
 }
 
 func showWinner() {
 	if hpHero > hpDragon {
 		fmt.Println("")
-		fmt.Println(checkLangShowText(setGetGameDataTextRU().winner, setGetGameDataTextEN().winner),
-			checkLangShowText(setGetGameDataTextRU().hero, setGetGameDataTextEN().hero), heroName)
+		fmt.Println(setGetGameDataText().winner, setGetGameDataText().hero, heroName)
 	} else if hpDragon > hpHero {
-		fmt.Println(checkLangShowText(setGetGameDataTextRU().winner, setGetGameDataTextEN().winner),
-			checkLangShowText(setGetGameDataTextRU().dragon, setGetGameDataTextEN().dragon))
+		fmt.Println(setGetGameDataText().winner, setGetGameDataText().dragon)
 	} else if hpDragon == hpHero {
-		fmt.Println(checkLangShowText(setGetGameDataTextRU().standoff, setGetGameDataTextEN().standoff))
+		fmt.Println(setGetGameDataText().standoff)
 	}
 }
 
 func showWeaponHero() {
 	fmt.Println("\n")
-	fmt.Println(checkLangShowText(setGetGameDataTextRU().selWeapon, setGetGameDataTextEN().selWeapon))
+	fmt.Println(setGetGameDataText().selWeapon)
 	weaponHero := [3]string{
-		checkLangShowText(setGetGameDataTextRU().weapon1, setGetGameDataTextEN().weapon1),
-		checkLangShowText(setGetGameDataTextRU().weapon2, setGetGameDataTextEN().weapon2),
-		checkLangShowText(setGetGameDataTextRU().weapon3, setGetGameDataTextEN().weapon3),
+		setGetGameDataText().weapon1,
+		setGetGameDataText().weapon2,
+		setGetGameDataText().weapon3,
 	}
 	for i := 0; i < len(weaponHero); i++ {
 		fmt.Println(weaponHero[i])
@@ -285,18 +206,18 @@ func attackToDragon() {
 		randomized := randomize(20, 40)
 		casesAttackToDragon(randomized, harmOfFrstn)
 	default:
-		fmt.Println(checkLangShowText(setGetMainMenuTextRU().incorrectInp, setGetMainMenuTextEN().incorrectInp))
+		fmt.Println(setGetMainMenuText().incorrectInp)
 	}
 }
 
 func casesAttackToDragon(randomized int, harm int) {
 	hpHero = hpHero - randomized
 	hpDragon = hpDragon - harm
-	fmt.Println(checkLangShowText(setGetGameDataTextRU().harmHeroToDragon, setGetGameDataTextEN().harmHeroToDragon), harm)
+	fmt.Println(setGetGameDataText().harmHeroToDragon, harm)
 	if randomized == 0 {
-		fmt.Println(checkLangShowText(setGetGameDataTextRU().dragonMiss, setGetGameDataTextEN().dragonMiss))
+		fmt.Println(setGetGameDataText().dragonMiss)
 	} else {
-		fmt.Println(checkLangShowText(setGetGameDataTextRU().harmDragonToHero, setGetGameDataTextEN().harmDragonToHero), randomized)
+		fmt.Println(setGetGameDataText().harmDragonToHero, randomized)
 	}
 	fmt.Println("\n")
 }
